28a194dc843b31a169e263a352e76174fceecf22
'sunjae' <1019lsj@naver.com>
2023-03-07 16:55:06 +0900

    23.03.07

--- spring.web/src/main/java/com/my/spring/web/ch02/ex01/UserController.java
+++ spring.web/src/main/java/com/my/spring/web/ch02/ex01/UserController.java
@@ -3,6 +3,7 @@ package com.my.spring.web.ch02.ex01;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -24,5 +25,25 @@ public class UserController {
 	@GetMapping("22")
 	public String handler22() {
 		return "ch02/ex01/user";
+	}
+	
+	@GetMapping("ch02/ex01/31")
+	public void handler31(User user) {
+		user.setUsername("양승일");
+		user.setAge(31);
+	}
+	
+	@GetMapping("ch02/ex01/32")
+	public void handler32(@ModelAttribute("man") User user) {
+		user.setUsername("서준한");
+		user.setAge(32);
+	}
+	
+	@GetMapping("ch02/ex01/41")
+	public User handler41(User user) {
+		user.setUsername("김가람");
+		user.setAge(21);
+		
+		return user;
 	}
 }
---
+++ spring2.core/src/main/java/spring2/core/ch02/ex03/Calculator.java
@@ -0,0 +1,15 @@
+package spring2.core.ch02.ex03;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Calculator {
+	public int calc(int num) {
+		try {
+			Thread.sleep((long)(Math.random() * 1000));
+		} catch(Exception e) {}
+		
+		System.out.println("Calculator.calc()");
+		return num;
+	}
+}
